416fdf5255fdb9bcd311bbf0700ab3e51da5df65
gyotaeklee <leegyotaek@gmail.com>
2021-01-24 22:45:30 +0900

    add CRUD Method on controller and viewTemplate pages

--- src/main/java/com/hitaek/springboot/crudepractice/controller/EmployeeController.java
+++ src/main/java/com/hitaek/springboot/crudepractice/controller/EmployeeController.java
@@ -6,9 +6,7 @@ import com.hitaek.springboot.crudepractice.entity.Employee;
 import com.hitaek.springboot.crudepractice.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -32,7 +30,47 @@ public class EmployeeController {
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 		
-		return "list-employees";
+		return "employees/list-employees";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel){
+
+		// create model attribute to bind form data
+		Employee employee = new Employee();
+
+		theModel.addAttribute("employee", employee);
+
+		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+
+		employeeService.save(theEmployee);
+
+		return "redirect:/employees/list";
+
+
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForAdd(@RequestParam("employeeId") int theId,
+								 Model theModel){
+
+		// create model attribute to bind form data
+		Employee employee = employeeService.findById(theId);
+
+		theModel.addAttribute("employee", employee);
+
+		return "employees/employee-form";
+	}
+
+	@GetMapping("/delete")
+	public String deleteEmployee(@RequestParam("employeeId") int theId){
+		employeeService.deleteById(theId);
+
+		return "redirect:/employees/list";
 	}
 }
 
--- src/main/java/com/hitaek/springboot/crudepractice/dao/EmployeeRepository.java
+++ src/main/java/com/hitaek/springboot/crudepractice/dao/EmployeeRepository.java
@@ -6,8 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+
+    public List<Employee> findAllByOrderByLastNameAsc();
+
+
 
 }
 
